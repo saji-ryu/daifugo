32b1a7a9e7f655cd3db549168718b97544158167
saji-ryu <rsajiki@gmail.com>
2019-03-10 00:21:21 +0900

    debugを充実

--- src/server/routes/debug.ts
+++ src/server/routes/debug.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import _debug from "debug";
 
-import { userData } from "../Memory";
+import { userData, roomData, tableData } from "../Memory";
 
 const router = express.Router();
 const debug = _debug("route:debug");
 
-router.get("/", (req, res) => {
+router.get("/user", (req, res) => {
   res.send(userData);
+});
+
+router.get("/room", (req, res) => {
+  res.send(roomData);
+});
+router.get("/table", (req, res) => {
+  res.send(tableData);
 });
 
 export default router;
